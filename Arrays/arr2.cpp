f7fbedeac03df1c0ed5ef8f167d16e2c272483de
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2021-08-24 15:51:12 +0000

    second largest number in an array

--- Arrays/arr2.cpp
+++ Arrays/arr2.cpp
@@ -26,6 +26,24 @@
         if(arr[i]>arr[l]) l=i;
       }
       return arr[l];
-    } //Time Complexity -->
+    } //Time Complexity --> Θ(n)
+
+//Second Largest Number in the array
+  int slargest(int arr[],int n){
+    int res=-1, largest=0;
+    for(int i=0;i<n;i++){
+      if(arr[i]>arr[largest]){
+        res=largest;
+        largest=i;        
+      }
+      else if(arr[i]<arr[largest]){
+        if(res==-1||arr[i]>arr[res]) res=i;
+      }
+    }
+    return arr[res]; 
+  } //Time Complexity --> Θ(n)
+
+  
+  
 
   
