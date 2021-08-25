73ce523e66ff3e01a2cc09481b765a370f6dbff7
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2021-08-25 15:05:21 +0000

    Binary Solution standard code: iiterative and recursive solution

---
+++ Searching/bin_srch.cpp
@@ -0,0 +1,20 @@
+//Binary Search
+  //Itterative Solution
+    int bin_srch(int arr[],int x,int N){
+      int start=0, end=N-1;
+      while(start<=end){
+        int mid=(start+end)/2;
+        if(arr[mid]==x) return mid;
+        else if(arr[mid]>x) end=mid-1;
+        else start=mid+1;
+      }
+      return -1;
+    }
+  //Recursive Solution
+  int bin_srch(int arr[],int x,int start, int end){
+    if(start>end) return -1; 
+    int mid=(start+end)/2;
+   if(arr[mid]==x) return mid;
+   else if(arr[mid]>x) return bin_srch(arr,start,mid-1,x);
+   else return bin_srch(arr, mid+1, end,x); 
+  }
