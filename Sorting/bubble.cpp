485ab48110f452591ce46d601f6cee9f8f289c64
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2021-08-27 11:32:45 +0530

    Update and rename quick.cpp to bubble.cpp
    bubble sort

--- Sorting/bubble.cpp
+++ Sorting/bubble.cpp
@@ -1,5 +1,5 @@
 // Quick Sort
-   int quicksort(int arr[], int n){
+   int bubblesort(int arr[], int n){
      for(int i=0;i<n-1;i++){ //loop for passes
        for(int j=0; j<n-1;j++){ //loop for array index
          if (arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
@@ -7,7 +7,7 @@
      }
    }
    //Optmised code
-    int quickSort(int arr[],int n){
+    int bubbleSort(int arr[],int n){
       for(int i=0; i<n-1;i++){
         for(int j=0;j<(n-1)-i;j++){
           if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
@@ -17,7 +17,7 @@
     //Since an element is already fixed at a position after every pass, we don't need to check it to traverse to that position.
   
   //Further Optimisation
-    int qsort(int arr[],int n){
+    int bsort(int arr[],int n){
       for(int i=0; i<n-1;i++){
         bool swapped=false; //To check if the array is already sorted or got sorted at an instance while executing the loop
         for(int j=0;j<(n-1)-i;j++){
@@ -30,4 +30,4 @@
       }
     }
     //If the array is already sorted or the array got sorted in the middle
-  
+   
