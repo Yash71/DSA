c7e61f930180e1014a39c4580be81fd729b8cc2e
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2021-11-04 21:07:53 +0000

    operations in Linked List

--- Linked List/Operations in Linked list/insertion3_ll.cpp
+++ Linked List/Operations in Linked list/insertion3_ll.cpp
@@ -1,3 +1,4 @@
+// Inserting after a particular(kth) position in a linked list
 #include<iostream>
 using namespace std;
 class LinkedList{
@@ -26,35 +27,41 @@ class LinkedList{
       tail=tail->next;
     }
   }
-  //deleting the node in a linked list
-  void deletion_list(int target_data){
-    node* current=head;
-    if(head==NULL){
+  bool empty_list(){
+    return head==NULL;
+  }
+  void insertion_k_list(int k, int data){
+    if(k<0){
+      cout<<"Invalid position"<<"\n";
+      return;
+    }
+    if(empty_list()){ //empty list
       cout<<"List is empty"<<"\n";
       return;
     }
-    else if(head->data==target_data){ //deleting head
-      head=head->next; 
-      free(current);
+    node* newnode=new node(data);
+    if(k==0){ //insertion at head
+      newnode->next=head;
+      head=newnode;
+      return;
     }
     else{
-      while(current->next!=NULL && current->next->data!=target_data){
+      node* current=head;
+      while(current!=NULL && --k){
         current=current->next;
       }
-      if(current->next==NULL){
-        cout<<"Data doesn't exists"<<"\n";
-        return;
+      if(k==0){
+        newnode->next=current->next;
+        current->next=newnode;
       }
       else{
-        node* target_node=current->next;
-        current->next=current->next->next;
-        free(target_node);
+        cout<<"Position exceeds the size of linked list"<<"\n";
       }
     }
   }
   void print_list(){
     if(head==NULL){
-      cout<<"the list is empty"<<"\n";
+      cout<<"List is empty"<<"\n";
       return;
     }
     node* current=head;
@@ -64,23 +71,25 @@ class LinkedList{
     }
     cout<<"NULL"<<"\n";
   }
-
 };
 int main(){
   LinkedList ll=LinkedList();
   ll.insertion_list(10);
   ll.insertion_list(20);
+  ll.insertion_list(30);
   ll.insertion_list(40);
-  ll.insertion_list(50);
-  ll.print_list();
-  ll.deletion_list(50); //deleting tail
   ll.print_list();
 
-  ll.deletion_list(20); //deleting intermediate node
+  ll.insertion_k_list(0,100); //insertion at head
   ll.print_list();
 
-  ll.deletion_list(10); //deleting head
+  ll.insertion_k_list(-1,2); // invalid insertion
   ll.print_list();
-  
 
+  ll.insertion_k_list(2,200); //insertion after kth position
+  ll.print_list();
+
+  ll.insertion_k_list(6,300); //insertion at tail
+  ll.print_list();
+  return 0;
 }
