28c4be248af5e8987405b8d5195fe0f634480518
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2021-11-11 07:11:07 +0000

    Operations in a DLL

---
+++ Linked List/Doubly Linked List/Operations in DLL/insertion_beg.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+  struct node{
+    int data;
+    node* next;
+    node* previous;
+    node(int d){
+      data=d;
+      next=NULL;
+      previous=NULL;
+    }
+  };
+node* insertion_beg(node* head,int data){
+  node* newnode=new node(data);
+  newnode->next=head;
+//   newnode->previous=NULL;
+  if(head!=NULL){
+    head->previous=newnode;
+  }
+  return newnode;
+}
+void print(node* head){
+  node* current=head;
+  while(current!=NULL){
+    cout<<current->data<<" ";
+    current=current->next;
+  }
+  cout<<"NULL"<<"\n";  
+}
+int main(){
+//   node *head=new node(10);
+// 	node *temp1=new node(20);
+// 	node *temp2=new node(30);
+// 	head->next=temp1;
+// 	temp1->previous=head;
+// 	temp1->next=temp2;
+// 	temp2->previous=temp1;
+    node* head=NULL;
+  head=insertion_beg(head,5);
+  print(head);
+  return 0;
+}
