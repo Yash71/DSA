8321b57eebc372bdebc510113d14e1e6fa8054d1
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2021-11-11 05:57:01 +0000

    Introduction to Doubly Linked List

--- Linked List/Doubly Linked List/intro_dll.cpp
+++ Linked List/Doubly Linked List/intro_dll.cpp
@@ -10,6 +10,14 @@ using namespace std;
       previous=NULL;
     }
   };
+  void print(node* head){
+    node* current=head;
+    while(current!=NULL){
+      cout<<current->data<<" ";
+      current=current->next;
+    }
+    cout<<"NULL"<<"\n";
+  }
 int main(){
   node* head= new node(10);
   node* temp1=new node(20);
@@ -18,5 +26,7 @@ int main(){
   temp1->previous=head;
   temp1->next=temp2;
   temp2->previous=temp1;
+
+  print(head);
   return 0;
 }
