c7fcd1044b91b42206ea8ac5dd43aa4887eaee0e
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2021-11-27 17:57:20 +0000

    queue in STL

---
+++ Queue/queue_stl.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(){
+  queue<int>q;
+  q.empty(); //checks if the queue is empty
+  q.size(); //returns the size of the queue
+  q.front(); //returns a reference to the first element
+  q.back(); //returns a reference to the last element
+  q.push(x); //adds the element to the top of the queue
+  q.pop(); //deletes the topmost element from the queue
+  
+}
