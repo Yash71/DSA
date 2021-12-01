76156fade34379e1ea48975a88fdc9eb6a4d123f
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2021-12-01 06:31:54 +0000

    Deque introduction

---
+++ Deque/introduction_dq.cpp
@@ -0,0 +1,15 @@
+// Built-in functions for deque in default library
+// empty(): checks if the queue is empty
+// size(): returns size of queue
+// front(): returns ref of first element
+// back(): returns ref of last element
+// push_front(x): insert element x to the front
+// push_back(x): insert element x to the back
+// pop_front(): deletes element from front
+// pop_back(): deletes element from rear
+
+//STL
+//insert(it,n): insert item at position pointed by the iterator
+//erase(it): removes item pointed by the itterator
+//begin(): returns an iterator ref to first item
+//end(): returns an iterator ref to the next of last item
