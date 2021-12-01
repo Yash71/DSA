e708dd5370bfa76cb3c9c52781dcbd938f76dee3
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2021-12-01 14:41:44 +0000

    inorder traversal in trees

---
+++ Trees/in_order.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+struct node{
+  int data;
+  node* left;
+  node* right;
+  node(int d){
+    data=d;
+    left=NULL;
+    right=NULL;
+  }
+};
+void inorder(node* root){
+  if(root==NULL)
+    return;
+  inorder(root->left);
+  cout<<root->data<<" ";
+  inorder(root->right);
+}
+int main(){
+  node* root=new node(10);
+  root->left=new node(20);
+  root->right=new node(30);
+  root->left->right=new node(40);
+  root->left->left=new node(50);
+  root->right->left=new node(60);
+  inorder(root);
+}
