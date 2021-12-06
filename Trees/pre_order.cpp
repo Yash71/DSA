de61aa62402b5400d651190be2e420c785a899eb
Gladingi_BOi <59257813+Yash71@users.noreply.github.com>
2021-12-06 06:49:36 +0000

    preorder traversal in trees

---
+++ Trees/pre_order.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+struct node{
+  int data;
+  node* left;
+  node* right;
+  node(int d){
+    data=d;
+    left=NULL;
+    right=NULL;
+  }
+};
+void preorder(node* root){
+  if(root==NULL) return;
+  cout<<root->data<<" ";
+  preorder(root->left);
+  preorder(root->right);  
+}
+int main(){
+  node* root=new node(10);
+  root->left=new node(20);
+  root->right=new node(30);
+  root->left->left=new node(40);
+  root->left->right=new node(50);
+  root->right->left=new node(60);
+  root->right->right=new node(70);
+  preorder(root);
+}
