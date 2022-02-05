ef4c8bb22a9b5352381b6394e75d9ecb71c8039e
yash <yashrajesh71@gmail.com>
2022-02-06 03:30:01 +0530

    floor of the number in the given array

---
+++ Searching/floor.cpp
@@ -0,0 +1,30 @@
+// find the ceiling of a target in the sorted array
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {2, 3, 5, 9, 14, 16, 18};
+    int n = 7;
+    int target = 15;
+    int ceil_value = -1;
+    int low = 0;
+    int high = n - 1;
+    bool flag = false;
+    while (low <= high)
+    {
+        int middle = (low + high) / 2;
+        if (arr[middle] == target)
+        {
+            flag = true;
+            ceil_value = arr[middle];
+            break;
+        }
+        if (arr[middle] > target)
+            high = middle - 1;
+        else
+            low = middle + 1;
+    }
+    flag ? cout << ceil_value : cout << arr[high];
+    return 0;
+}
