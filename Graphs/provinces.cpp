5d3c1637cfcbfd0c2b958535aa143eb5be220393
gladingi <yashrajesh71@gmail.com>
2024-04-15 02:57:12 +0530

    added problem link and solution

--- Graphs/provinces.cpp
+++ Graphs/provinces.cpp
@@ -1,6 +1,32 @@
 #include<iostream>
 using namespace std;
-
+/*	Question Link: https://www.geeksforgeeks.org/problems/number-of-provinces/1
+	Approach: build a visited array and mark every node as 0(not visited), now traverse the visited array and for every unvisited array as 0, call the dfs function 
+*/
+void dfs(int start,vector<int>adjList[],int vis[]){
+	vis[start]=1;
+	for(auto it: vis[start]){
+		if(!vis[it]) dfs(it, adjList,vis);
+	}
+}
 int main(){
-	
+	vector<int>adjList[V];
+	for(int i=0;i<V;i++){
+		for(int j=0;j<V;j++){
+			if(adj[i][j]==1 && i!=k){
+				adjList[i].push_back(j);
+				adjList[j].push_back(i);
+			}
+		}
+	}
+	int vis[V]={0};
+	int provinces=0;
+	for(int i=0;i<V;i++){
+		if(!vis[i]){
+			provinces++;
+			dfs(i,adjList,vis);
+		}
+	}
+	return provinces;
 	return 0;
+}
